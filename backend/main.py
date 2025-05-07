4a277169a0507a313c0fbd29d894f59228350ebc
Sovok00 <158320974+Sovok00@users.noreply.github.com>
2025-05-07 14:03:49 +0300

    Update and rename taskFlow_first to main.py

--- backend/main.py
+++ backend/main.py
@@ -18,12 +18,12 @@ tasks_db: List[Task] = [
 
 # Получить все задачи
 @app.get("/tasks", response_model=List[Task])
-def get_tasks():
+async def get_tasks():
     return tasks_db
 
 # Добавить задачу
 @app.post("/tasks", response_model=Task)
-def create_task(task: Task):
+async def create_task(task: Task):
     new_task = Task(id=len(tasks_db) + 1, title=task.title, done=task.done)
     tasks_db.append(new_task)
     return new_task
