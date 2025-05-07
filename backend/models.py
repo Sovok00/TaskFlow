fdf356e5452d0b6a949a8406da58412f24485813
Sovok00 <158320974+Sovok00@users.noreply.github.com>
2025-05-07 14:11:59 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,12 @@
+from pydantic import BaseModel
+
+class Task(BaseModel):
+    id: int
+    title: str
+    done: bool = False
+
+class TaskCreate(BaseModel):
+  title: str
+  done: bool = False
+
 
